1589e5a3b8f06b4e1d5f881bf1bd5973ff3974e9
Alain Pham <alainphm@gmail.com>
2023-10-29 09:15:36 +0100

    hostfile for windows

---
+++ playbooks/local/hostfile.sh
@@ -0,0 +1,49 @@
+# Copyright (c) 1993-2009 Microsoft Corp.
+#
+# This is a sample HOSTS file used by Microsoft TCP/IP for Windows.
+#
+# This file contains the mappings of IP addresses to host names. Each
+# entry should be kept on an individual line. The IP address should
+# be placed in the first column followed by the corresponding host name.
+# The IP address and the host name should be separated by at least one
+# space.
+#
+# Additionally, comments (such as these) may be inserted on individual
+# lines or following the machine name denoted by a '#' symbol.
+#
+# For example:
+#
+#      102.54.94.97     rhino.acme.com          # source server
+#       38.25.63.10     x.acme.com              # x client host
+
+# localhost name resolution is handled within DNS itself.
+#	127.0.0.1       localhost
+#	::1             localhost
+172.22.138.118 aphm.duckdns.org
+172.22.138.118 traefik.aphm.duckdns.org
+172.22.138.118 portainer.aphm.duckdns.org
+172.22.138.118 registry.aphm.duckdns.org
+172.22.138.118 regui.aphm.duckdns.org
+172.22.138.118 keycloak.aphm.duckdns.org
+172.22.138.118 reposilite.aphm.duckdns.org
+
+172.22.138.118 minio.aphm.duckdns.org
+172.22.138.118 adminer.aphm.duckdns.org
+
+172.22.138.118 kafdrop.aphm.duckdns.org
+172.22.138.118 artemis-console.aphm.duckdns.org
+
+
+
+172.22.138.118 cadvisor.aphm.duckdns.org
+172.22.138.118 mimir.aphm.duckdns.org
+172.22.138.118 loki.aphm.duckdns.org
+172.22.138.118 tempo.aphm.duckdns.org
+172.22.138.118 prometheus.aphm.duckdns.org
+172.22.138.118 grafana-agent.aphm.duckdns.org
+172.22.138.118 grafana.aphm.duckdns.org
+172.22.138.118 grafana-enterprise.aphm.duckdns.org
+
+172.22.138.118 smoke-test-app.aphm.duckdns.org
+
+
